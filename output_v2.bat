35ac759839e0a862dfaf3831f3c726f1a2d567a5
TakamiChie@Marduk <chie@onpu-tamago.net>
2017-05-25 14:26:32 +0900

    出力フォルダを作成

--- output_v2.bat
+++ output_v2.bat
@@ -1,7 +1,7 @@
 @echo off
 SET DETAILFILE=%OneDrive%\広報チーム\イベントカレンダー\イベント詳細一覧表.xlsx
 echo HTMLを出力しています...
-python .\src\main.py "%1" -e "%DETAILFILE%" -t doc -o out.html
+python .\src\main.py "%1" -e "%DETAILFILE%" -t doc -o .\out\out.html
 echo Googleカレンダー用CSVファイルを出力しています...
-python .\src\main.py "%1" -e "%DETAILFILE%" -t googlecsv -o googlecsv.csv
+python .\src\main.py "%1" -e "%DETAILFILE%" -t googlecsv -o .\out\googlecsv.csv
 echo 処理が完了しました
